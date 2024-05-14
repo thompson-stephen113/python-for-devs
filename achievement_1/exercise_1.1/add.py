70c0c2dbfbdd9d254ec9adefebe78f28d6ff97fd
Stephen Thompson <thompson.stephen113@gmail.com>
2024-05-14 13:04:46 -0400

    Initial commit

---
+++ achievement_1/exercise_1.1/add.py
@@ -0,0 +1,11 @@
+# First user input
+a = int(input("Enter a number. \n"))
+
+# Second user input
+b = int(input("Enter another number. \n"))
+
+# Sum of inputs
+c = a + b
+
+# Print sum
+print(str(a) + " + " + str(b) + " = " + str(c))
